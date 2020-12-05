929404bbd060d58ca3e49dcf05110e42f47c925d
Luke Henry <henryluke127@gmail.com>
2020-12-04 18:32:48 -0700

    Changes made

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,6 +6,16 @@ var MessagesView = {
   },
 
   render: function() {
+  },
+
+  renderMessage: function(message) {
+
+    var test = MessageView.render(message);
+    console.log(test);
+    //console.log(MessagesView.$chats);
+
+    // adds message to $chats.children()
+    MessagesView.$chats.append(test);
   }
 
 };
