e94e7899e7822f3c9f78ba6b767ca58b82b70799
Luke Henry <henryluke127@gmail.com>
2020-12-05 17:17:48 -0700

    Working on formView

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,4 +1,8 @@
 var Messages = {
 
-
 };
+
+// this.username = App.username;
+// this.text = $('#message').val();
+// this.roomname = $('#rooms').val();
+
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -3,6 +3,8 @@ var RoomsView = {
   $button: $('#rooms button'),
   $select: $('#rooms select'),
 
+
+
   initialize: function() {
   },
 
